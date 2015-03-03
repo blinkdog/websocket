50130826a9cb1a32a6e6899a099b571d8b1e69a6
no-glue <nikolapav1985@gmail.com>
2015-03-03 13:42:17 +0100

    moved web socket consumer thread to different file

--- src/main/java/com/pmeade/websocket/example/EchoServerSync.java
+++ src/main/java/com/pmeade/websocket/example/EchoServerSync.java
@@ -3,6 +3,7 @@ package com.pmeade.websocket.example;
 import com.pmeade.websocket.io.WebSocketServerOutputStream;
 import com.pmeade.websocket.net.WebSocket;
 import com.pmeade.websocket.net.WebSocketServerSocket;
+import com.pmeade.websocket.example.WebSocketConsumerThread;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
@@ -108,45 +109,6 @@ class WebSocketThread extends Thread {
     private final WebSocket webSocket;
     private StringMessageQueue messageQueue;
     private ByteAccumulator buffer;
-}
-
-/**
- * web socket consumer thread
- * takes messages from message queue and sends them to clients
- */
-class WebSocketConsumerThread extends Thread {
-  public WebSocketConsumerThread(StringMessageQueue messageQueue, LinkedList<WebSocket> connections) {
-    this.messageQueue = messageQueue;
-    this.connections = connections;
-  }
-  public void run() {
-    String message = "";
-    WebSocket webSocket = null;
-    WebSocketServerOutputStream wsos = null;
-    while(!finished) {
-      try {
-        message = messageQueue.pop();
-        ListIterator<WebSocket> listIterator = connections.listIterator();
-        while(listIterator.hasNext()) {
-          webSocket = listIterator.next();
-          wsos = webSocket.getOutputStream();
-          wsos.writeString(message);
-        }
-      } catch(IOException e) {
-        finished = true;
-        System.err.println(e.getLocalizedMessage());
-        e.printStackTrace(System.err);
-      }
-      catch(InterruptedException e) {
-        finished = true;
-        System.err.println(e.getLocalizedMessage());
-        e.printStackTrace(System.err);
-      }
-    }
-  }
-  private StringMessageQueue messageQueue;
-  private LinkedList<WebSocket> connections;
-  private boolean finished = false;
 }
 
 /**
